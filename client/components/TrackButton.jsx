f238034bee3e2e0712b2f8aa50173aa593189a39
Finn-Johansson <astro_johansson@hotmail.com>
2022-05-05 15:01:23 +1200

    create skellingtons of components ;

---
+++ client/components/TrackButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function TrackButton() {
+  return <div>TrackButton</div>
+}
+
+export default TrackButton
---
+++ client/components/UserTracks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function UserTracks() {
+  return <div>UserTracks</div>
+}
+
+export default UserTracks
