02126e41f8c029286271e8ed7eff9d9711493f22
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-05 21:49:46 +1200

    add routes to add to saved/ completed tracks to db

--- server/db/tracks.js
+++ server/db/tracks.js
@@ -4,6 +4,16 @@ function listTracks(db = connection) {
   return db('track_data').select()
 }
 
+function addSavedTrack({ userId, trackId }, db = connection) {
+  const savedTrack = { user_id: userId, track_id: trackId }
+  return db('saved_tracks').insert(savedTrack)
+}
+
+function addCompletedTrack({ userId, trackId }, db = connection) {
+  const completedTrack = { user_id: userId, track_id: trackId }
+  return db('completed_tracks').insert(completedTrack)
+}
+
 function getTrackById(id, db = connection) {
   return db('track_data')
     .where('id', id)
@@ -22,4 +32,6 @@ function getTrackById(id, db = connection) {
 module.exports = {
   getTrackById,
   listTracks,
+  addSavedTrack,
+  addCompletedTrack,
 }
--- server/routes/tracks.js
+++ server/routes/tracks.js
@@ -18,17 +18,51 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/saved', (req, res) => {
+  const { userId, trackId } = req.body
+  const savedTrack = {
+    userId,
+    trackId,
+  }
+  db.addSavedTrack(savedTrack)
+    .then(() => {
+      res.sendStatus(201)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Unable to save track' })
+    })
+})
+
+router.post('/completed', (req, res) => {
+  const { userId, trackId } = req.body
+  const completedTrack = {
+    userId,
+    trackId,
+  }
+  db.addcompletedTrack(completedTrack)
+    .then(() => {
+      res.sendStatus(201)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Unable to save track as completed' })
+    })
+})
+
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id)
   db.getTrackById(id)
-  .then((tracks) => {
-    res.json(tracks)
-    return null
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong' })
-  })
+    .then((tracks) => {
+      res.json(tracks)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 module.exports = router
