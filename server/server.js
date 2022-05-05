64a042b3b9aa213f5888b13175daba672b889b9a
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-05 16:05:06 +1200

    add badges routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const usersRoutes = require('./routes/users')
+const badgesRoutes = require('./routes/badges')
 
 const server = express()
 
@@ -9,6 +10,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/users', usersRoutes)
+server.use('/api/v1/badges', badgesRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
