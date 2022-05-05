62634d00a97a13d55230f3b298faf2ad811a930d
Clementine <cmwsmart@gmail.com>
2022-05-05 23:12:36 +1200

    Add leaflet map component with path line

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ import PingRoutes from './PingRoutes'
 import Registration from './Registration'
 import Users from './Users'
 import { Routes, Route } from 'react-router-dom'
+import Map from './Map'
 
 function App() {
   cacheUser(useAuth0)
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Users />} />
         <Route path="/" element={<PingRoutes />} />
         <Route path="/profile" element={<Registration />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </div>
   )
