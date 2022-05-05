f55090576c3578a42c22f1690fcce9f379ae3f0e
Jesse Reeves <jessefretless@gmail.com>
2022-05-05 20:41:18 +1200

    build route to show saved tracks by user

--- server/db/tracks.js
+++ server/db/tracks.js
@@ -19,7 +19,15 @@ function getTrackById(id, db = connection) {
     .first()
 }
 
+function getSavedTrackByUser(userId, db = connection) {
+  return db('saved_tracks')
+  .where('user_id', userId)
+  .join('track_data', 'track_data.id', 'saved_tracks.track_id' )
+  .select('track_id')
+}
+
 module.exports = {
   getTrackById,
   listTracks,
+  getSavedTrackByUser
 }
--- server/routes/tracks.js
+++ server/routes/tracks.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 // GET /api/v1/tracks
 
+// List all tracks
 router.get('/', (req, res) => {
   db.listTracks()
     .then((tracks) => {
@@ -18,17 +19,32 @@ router.get('/', (req, res) => {
     })
 })
 
+// Get track by ID
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id)
   db.getTrackById(id)
-  .then((tracks) => {
-    res.json(tracks)
-    return null
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong' })
-  })
+    .then((tracks) => {
+      res.json(tracks)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+// Get saved track by user ID
+router.get('/saved/:userId', (req, res) => {
+  const userId = Number(req.params.userId)
+  db.getSavedTrackByUser(userId)
+    .then((tracks) => {
+      res.json(tracks)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 module.exports = router
