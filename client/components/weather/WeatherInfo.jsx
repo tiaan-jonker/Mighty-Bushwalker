ed15ebf15381d1c3028d89bde3970110262cd811
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-06 03:30:10 +1200

    weather component functions changed and mapped data added

--- client/components/weather/WeatherInfo.jsx
+++ client/components/weather/WeatherInfo.jsx
@@ -1,33 +1,63 @@
 import React, { useState, useEffect } from 'react'
+import {
+  faCloud,
+  faBolt,
+  faCloudRain,
+  faCloudShowersHeavy,
+  faSnowflake,
+  faSun,
+  faSmog,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { WEATHER_API_KEY } from '../../../weather'
 
 function WeatherInfo() {
-  const [data, setData] = useState([])
+  const [weatherData, setWeatherData] = useState([])
 
   const lat = '-36.212791'
   const lon = '175.402013'
-
-  const { weather, main, wind, sys } = data
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
 
   useEffect(() => {
-    const fetchWeatherData = async() => {
-      await fetch(url)
-        .then((res) => res.json())
-        .then((result) => {
-          setData(result)
-        })
-    }
     fetchWeatherData()
+      .then((weather) => {
+        setWeatherData(weather)
+        console.log(weatherData)
+      })
+      .catch((err) => console.error(err))
   }, [])
 
-  console.log(sys)
+  const handleResponse = (response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('Please Enable your Location in your browser!')
+    }
+  }
 
-  return (
-    <div>
-      <div>
-        {/* <p>{weather[0].description}</p> */}
-      </div>
-    </div>
-  )
+  const fetchWeatherData = () => {
+    return fetch(url)
+      .then((response) => handleResponse(response))
+      .then((weather) => {
+        if (Object.entries(weather).length) {
+          const mappedData = mapDataToWeatherInterface(weather)
+          return mappedData
+        }
+      })
+  }
+
+  function mapDataToWeatherInterface(weatherData) {
+    const mapped = {
+      description: weatherData.weather[0].main,
+      currentTemperature: Math.round(weatherData.main.temp),
+      sunrise: new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
+        'en-IN'
+      ),
+    }
+    return mapped
+  }
+  
+  return <div>{}</div>
 }
 
 export default WeatherInfo
