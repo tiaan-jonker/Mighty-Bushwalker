63f23b47bfa701ccdab78d4cd4c218012b447c92
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-06 15:51:13 +1200

    add new user_tracks seed

---
+++ server/db/seeds/user_tracks.js
@@ -0,0 +1,22 @@
+exports.seed = function (knex) {
+  const amountOfUsers = 2
+  const amountOfTracks = 20
+
+  const userTracks = []
+
+  for (let i = 1; i < amountOfUsers + 1; i++) {
+    for (let j = 1; j < amountOfTracks + 1; j++) {
+      userTracks.push({
+        user_id: i,
+        track_id: j,
+        completed: false,
+        saved: false,
+      })
+    }
+  }
+  return knex('user_tracks')
+    .del()
+    .then(function () {
+      return knex('user_tracks').insert(userTracks)
+    })
+}
