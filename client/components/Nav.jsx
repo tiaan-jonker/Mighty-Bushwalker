e819b14a36eb14d4f3e09217875bc991d5c91c5d
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-07 11:07:28 +1200

    change text colour

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -73,7 +73,9 @@ function Nav() {
                 fill={setProfileColor()}
               />
             </svg>
-            <p className="nav-text">Profile</p>
+            <p className="nav-text" style={{ color: setProfileColor() }}>
+              Profile
+            </p>
           </NavLink>
         </div>
         <div className="nav-icon-container">
@@ -90,7 +92,9 @@ function Nav() {
                 fill={setTracksColor()}
               />
             </svg>
-            <p className="nav-text">My tracks</p>
+            <p className="nav-text" style={{ color: setTracksColor() }}>
+              My tracks
+            </p>
           </NavLink>
         </div>
         <div className="nav-icon-container">
@@ -106,7 +110,9 @@ function Nav() {
               <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
             </svg>
 
-            <p className="nav-text">Explore</p>
+            <p className="nav-text" style={{ color: setExploreColor() }}>
+              Explore
+            </p>
           </NavLink>
         </div>
       </section>
