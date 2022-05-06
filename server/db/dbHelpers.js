6f761c1147329e792eda8e8369c06ea4b468f58c
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-06 21:40:13 +1200

    add randomisation to user track seed data

--- server/db/dbHelpers.js
+++ server/db/dbHelpers.js
@@ -24,7 +24,7 @@ function generateUserTrackDataSeed() {
   // seed data is a row for each combination of user_id and track_id with completed and saved set to zero (false)
   // e.g. for 2 users and three tracks
   // [{user_id: 1, track_id: 1, ...}, {user_id: 1, track_id: 2, ...}, {user_id: 1, track_id: 3, ...},
-  //  {user_id: 2, track_id: 1, ...},{user_id: 2, track_id: 2, ...}, {user_id: 2, track_id: 3, ...}]
+  //  {user_id: 2, track_id: 1, ...}, {user_id: 2, track_id: 2, ...}, {user_id: 2, track_id: 3, ...}]
 
   const amountOfUsers = 2
   const amountOfTracks = 20
@@ -33,11 +33,14 @@ function generateUserTrackDataSeed() {
 
   for (let i = 1; i < amountOfUsers + 1; i++) {
     for (let j = 1; j < amountOfTracks + 1; j++) {
+      const randSaved = Math.random() < 0.2
+      const randComplete = Math.random() < 0.2
+
       userTracks.push({
         user_id: i,
         track_id: j,
-        completed: 0,
-        saved: 0,
+        completed: randSaved,
+        saved: randComplete,
       })
     }
   }
