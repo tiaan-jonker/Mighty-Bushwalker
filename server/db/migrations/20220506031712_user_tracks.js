ff3060f091855f30efa65087d2e0d1c191c98cc6
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-06 15:22:12 +1200

    add user_tracks migration table

--- server/db/migrations/20220506031712_user_tracks.js
+++ server/db/migrations/20220506031712_user_tracks.js
@@ -1,10 +1,12 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('saved_tracks', (table) => {
+  return knex.schema.createTable('user_tracks', (table) => {
     table.integer('user_id').references('users.id')
     table.integer('track_id').references('track_data.id')
+    table.boolean('completed')
+    table.boolean('saved')
   })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('saved_tracks')
+  return knex.schema.dropTable('user_tracks')
 }
