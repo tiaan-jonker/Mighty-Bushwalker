944046c78c7ddaa991a71791d3028d1f8e3c86d8
Clementine-Smart <cmwsmart@gmail.com>
2022-05-06 15:34:00 +1200

    Add consume function to userHelper

--- client/components/user/userHelper.js
+++ client/components/user/userHelper.js
@@ -1,11 +1,27 @@
-import request from 'superagent'
+import requestor from '../../consume'
 
-const baseUrl = '/api/v1'
+// export function getUser(id) {
+//   return request.get(`${baseUrl}/users/${id}`).then((res) => res.body)
+// }
 
-export function getUser(id) {
-  return request.get(`${baseUrl}/users/${id}`).then((res) => res.body)
+export function getUser(id, consume = requestor) {
+  return consume(`/users/${id}`)
+    .then((res) => {
+      const user = res.body
+      return user
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
 }
 
-export function getUserBadges(id) {
-  return request.get(`${baseUrl}/badges/${id}`).then((res) => res.body)
+export function getUserBadges(id, consume = requestor) {
+  return consume(`/badges/${id}`)
+    .then((res) => {
+      const badges = res.body
+      return badges
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
 }
