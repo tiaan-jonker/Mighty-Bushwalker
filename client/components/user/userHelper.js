de0ab68cba8db59b4871b13296949529146f7ffe
Tiaan Jonker <tj@Tiaans-MBP.hub>
2022-05-06 14:40:13 +1200

    badges component basic functionality

---
+++ client/components/user/userHelper.js
@@ -0,0 +1,11 @@
+import request from 'superagent'
+
+const baseUrl = '/api/v1'
+
+export function getUser(id) {
+  return request.get(`${baseUrl}/users/${id}`).then((res) => res.body)
+}
+
+export function getUserBadges(id) {
+  return request.get(`${baseUrl}/badges/${id}`).then((res) => res.body)
+}
