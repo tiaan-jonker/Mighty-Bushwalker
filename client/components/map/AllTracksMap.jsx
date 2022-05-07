17b047cbcbd848878e27050a2b19032573d56a34
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-07 17:04:22 +1200

    adjust zoom

--- client/components/map/AllTracksMap.jsx
+++ client/components/map/AllTracksMap.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { listTracks } from '../../../server/db/tracks'
 
 function AllTrackMap({ tracks }) {
   const [lat, setLat] = useState(0)
@@ -21,7 +20,7 @@ function AllTrackMap({ tracks }) {
         <MapContainer
           className="all-map"
           center={[lat, long]} // This will change - with the long and lat passed in from the track
-          zoom={11}
+          zoom={8}
           scrollWheelZoom={true}
         >
           <TileLayer url="https://api.mapbox.com/styles/v1/clemenware/cl2sqqm2s000i14nzde71lqaj/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2xlbWVud2FyZSIsImEiOiJjbDJzcWpsMG0wMHFnM2pvYXVmNTh0dnE3In0.Gi3RGqZv_HHyNk8Es6Aojw" />
