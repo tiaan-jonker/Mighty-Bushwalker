463419e05f7c777f8ddc167f3d832bce05515668
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-08 22:29:07 +1200

    suntimes comp redux state added

--- client/components/weather/WeatherInfo.jsx
+++ client/components/weather/WeatherInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Weather from './Weather'
 import Suntimes from './Suntimes'
 import { Box, Tab } from '@mui/material'
