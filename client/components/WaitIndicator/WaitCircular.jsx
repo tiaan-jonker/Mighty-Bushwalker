9ae32e816520697e1ba68d47d432d4dfcf26742c
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-09 15:44:57 +1200

    waiting indicator added and arrow styling

--- client/components/WaitIndicator/WaitCircular.jsx
+++ client/components/WaitIndicator/WaitCircular.jsx
@@ -1,20 +1,13 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
 import CircularProgress from '@mui/material/CircularProgress'
 import Box from '@mui/material/Box'
 
-function WaitCircular(props) {
-  return props.waiting ? (
+function WaitCircular() {
+  return (
     <Box sx={{ display: 'flex' }}>
       <CircularProgress />
     </Box>
-  ) : null
+  )
 }
 
-function mapStateToProps(state) {
-  return {
-    waiting: state.waiting,
-  }
-}
-
-export default connect(mapStateToProps)(WaitCircular)
+export default WaitCircular
