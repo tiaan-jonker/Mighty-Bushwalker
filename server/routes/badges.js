ae319a5e64a817551afae54fa19b447ae6c7fba4
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-10 10:34:24 +1200

    add login in badge component to render all badges

--- server/routes/badges.js
+++ server/routes/badges.js
@@ -21,6 +21,7 @@ router.get('/:userId', async (req, res) => {
   const userId = Number(req.params.userId)
   try {
     const badges = await db.getBadgesByUser(userId)
+    console.log(badges)
     res.json(badges)
   } catch (error) {
     console.error(error)
