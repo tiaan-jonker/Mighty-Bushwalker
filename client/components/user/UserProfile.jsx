1c648ed549bceca41737c4ddfe186a10c5061899
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-09 21:49:44 +1200

    modal finsihed

--- client/components/user/UserProfile.jsx
+++ client/components/user/UserProfile.jsx
@@ -25,65 +25,74 @@ function UserProfile() {
   }
 
   return (
-    <section className="page-container container-bg-green">
-      <img
-        src="/icons/logout.svg"
-        alt=""
-        onClick={handleClick}
-        className="logout-btn"
-      />
-      {isOpenModal && (
-        <LogoutModal
-          handleLogout={handleLogout}
-          setIsOpenModal={setIsOpenModal}
+    <div>
+      <section className="page-container container-bg-green">
+        <img
+          src="/icons/logout.svg"
+          alt=""
+          onClick={handleClick}
+          className="logout-btn"
         />
-      )}
-
-      <div>
-        <h2 className="user-intro">Hello {user.name},</h2>
-        <h3 className="user-intro-sub">Ready to level up your walking?</h3>
-      </div>
-      <UserStats user={user} />
-      <div className="user-links-container">
-        <div className="link-one">
-          <div className="link-container-one">
-            <div className="link-text-container">
-              <div className="link-text">Explore</div>
-              <img src="/icons/arrow.svg" alt="" />
+        {isOpenModal && (
+          <LogoutModal
+            handleLogout={handleLogout}
+            setIsOpenModal={setIsOpenModal}
+          />
+        )}
+        <div>
+          <h2 className="user-intro">Hello {user.name},</h2>
+          <h3 className="user-intro-sub">Ready to level up your walking?</h3>
+        </div>
+        <UserStats user={user} />
+        <div className="user-links-container">
+          <div className="link-one">
+            <div className="link-container-one">
+              <div className="link-text-container">
+                <div className="link-text">Explore</div>
+                <img src="/icons/arrow.svg" alt="" />
+              </div>
+              <Link to="/tracks">
+                <img
+                  src="/images/explore-img.png"
+                  alt=""
+                  className="user-img-one"
+                />
+              </Link>
             </div>
-            <Link to="/tracks">
-              <img
-                src="/images/explore-img.png"
-                alt=""
-                className="user-img-one"
-              />
-            </Link>
+          </div>
+          <div className="link-two">
+            <div className="link-container">
+              <div className="link-text-container">
+                <div className="link-text">My tracks</div>
+                <img src="/icons/arrow.svg" alt="" />
+              </div>
+              <Link to={`/user/${id}/usertracks`}>
+                <img
+                  src="/images/mybadges-img.png"
+                  alt=""
+                  className="user-img"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="link-three">
+            <div className="link-container">
+              <div className="link-text-container">
+                <div className="link-text">My badges</div>
+                <img src="/icons/arrow.svg" alt="" />
+              </div>
+              <Link to={`/user/${id}/userbadges`}>
+                <img
+                  src="/images/mytracks-img.png"
+                  alt=""
+                  className="user-img"
+                />
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="link-two">
-          <div className="link-container">
-            <div className="link-text-container">
-              <div className="link-text">My tracks</div>
-              <img src="/icons/arrow.svg" alt="" />
-            </div>
-            <Link to={`/user/${id}/usertracks`}>
-              <img src="/images/mybadges-img.png" alt="" className="user-img" />
-            </Link>
-          </div>
-        </div>
-        <div className="link-three">
-          <div className="link-container">
-            <div className="link-text-container">
-              <div className="link-text">My badges</div>
-              <img src="/icons/arrow.svg" alt="" />
-            </div>
-            <Link to={`/user/${id}/userbadges`}>
-              <img src="/images/mytracks-img.png" alt="" className="user-img" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
