cf452f8e201c29f8b3a42ca3670b55ac16a80432
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-09 21:38:46 +1200

    logout modal implemented

--- client/components/user/UserProfile.jsx
+++ client/components/user/UserProfile.jsx
@@ -1,10 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { getLogoutFn } from '../../auth0-utils'
 import UserStats from './UserStats.jsx'
 import { useSelector } from 'react-redux'
-import { capitaliseFirstLetter } from '../../utils'
+import LogoutModal from './LogoutModal'
 
 function UserProfile() {
   const user = useSelector((state) => state.user)
@@ -12,14 +12,33 @@ function UserProfile() {
   const logout = getLogoutFn(useAuth0)
   const Navigate = useNavigate
 
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
   function handleLogout(event) {
     event.preventDefault()
     logout()
     Navigate('/')
   }
 
+  function handleClick() {
+    setIsOpenModal(true)
+  }
+
   return (
     <section className="page-container container-bg-green">
+      <img
+        src="/icons/logout.svg"
+        alt=""
+        onClick={handleClick}
+        className="logout-btn"
+      />
+      {isOpenModal && (
+        <LogoutModal
+          handleLogout={handleLogout}
+          setIsOpenModal={setIsOpenModal}
+        />
+      )}
+
       <div>
         <h2 className="user-intro">Hello {user.name},</h2>
         <h3 className="user-intro-sub">Ready to level up your walking?</h3>
@@ -64,7 +83,6 @@ function UserProfile() {
           </div>
         </div>
       </div>
-      <button onClick={handleLogout}>x</button>
     </section>
   )
 }
