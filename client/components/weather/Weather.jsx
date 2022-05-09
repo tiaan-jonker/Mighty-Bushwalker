26114da984d00a6a2b549364da52f87a39120480
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-09 12:28:52 +1200

    weather comp small refactor

--- client/components/weather/Weather.jsx
+++ client/components/weather/Weather.jsx
@@ -5,7 +5,7 @@ import { fetchForecast } from '../../actions/forecast'
 
 function Weather() {
   const weather = useSelector((state) => state.weather)
-  const weatherIcon = useSelector((state) => state.weatherIcon)
+  // const weatherIcon = useSelector((state) => state.weatherIcon)
   const forecast = useSelector((state) => state.forecast)
   const dispatch = useDispatch()
 
@@ -14,34 +14,23 @@ function Weather() {
     dispatch(fetchForecast())
   }, [])
 
-  const getIcon = async () => {
-    const weatherIconState = await weatherIcon
-
-    switch (weatherIconState) {
-      case 'Clouds':
-        return '/icons/weather/cloud.png'
-      case 'Thunderstorm':
-        return '/icons/weather/thunderstorm.png'
-      case 'Rain':
-        return '/icons/weather/rain.png'
-      case 'Drizzle':
-        return '/icons/weather/rain.png'
-      case 'Clear':
-        return '/icons/weather/sunny.png'
-    }
-  }
-
   return (
     <div>
       <div className="weather-container">
         <div className="weather-rectangle">
           <p className="weather-day">Today</p>
-          <img src={getIcon()} alt="" className="weather-icon" />
+          <img
+            src="/icons/weather/cloudy.png"
+            alt=""
+            className="weather-icon"
+          />
           <p className="weather-temp">{weather.temperature}&deg;C</p>
         </div>
         {forecast.slice(0, 2).map((forecastData, index) => (
           <div key={index} className="weather-rectangle">
-            <p className="weather-day">Tomorrow</p>
+            <p className="weather-day">
+              {index === 0 ? 'Tomorrow' : 'Saturday'}
+            </p>
             <img
               src="/icons/weather/rain.png"
               alt=""
