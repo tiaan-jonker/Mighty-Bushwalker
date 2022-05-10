acdbb6799b657d76c7e09273993ad232a4151ba3
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-10 22:06:41 +1200

    splash page animations

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { getLoginFn, getRegisterFn } from '../auth0-utils'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import MobileHome from './MobileHome'
+import SplashPage from './SplashPage'
 
 function Landing() {
   const user = useSelector((state) => state.user)
   const login = getLoginFn(useAuth0)
   const register = getRegisterFn(useAuth0)
+  const [home, setHome] = useState(false)
+  const [animation, setAnimation] = useState(false)
+
+  function handleClick() {
+    setAnimation(true)
+    setTimeout(() => {
+      setAnimation(false)
+      setHome(true)
+    }, 3000)
+  }
 
   function handleLogin(event) {
     event.preventDefault()
@@ -29,34 +41,46 @@ function Landing() {
       )}
 
       <IfNotAuthenticated>
-        <div className="register-container">
-          <div className="register-box">
-            <div className="sign-in">
-              <img src="/icons/BushWalkLogo.svg" />
-              <h1 className="register-text register-heading">
-                THE MIGHTY BUSH
-              </h1>
+        {home ? (
+          <>
+            <div className="register-container">
+              <div className="register-box">
+                <div className="sign-in">
+                  <img src="/icons/BushWalkLogo.svg" />
+                  <h1 className="register-text register-heading">
+                    THE MIGHTY BUSH
+                  </h1>
+                </div>
+                <div className="form-group">
+                  <button
+                    onClick={handleLogin}
+                    type="submit"
+                    id="btn-login"
+                    className="btn btn-primary btn-block rounded-btn"
+                  >
+                    Log In
+                  </button>
+                  <button
+                    onClick={handleRegister}
+                    type="button"
+                    id="btn-signup"
+                    className="btn btn-default btn-block"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="form-group">
-              <button
-                onClick={handleLogin}
-                type="submit"
-                id="btn-login"
-                className="btn btn-primary btn-block rounded-btn"
-              >
-                Log In
-              </button>
-              <button
-                onClick={handleRegister}
-                type="button"
-                id="btn-signup"
-                className="btn btn-default btn-block"
-              >
-                Sign Up
-              </button>
-            </div>
+          </>
+        ) : (
+          <div className="mobile-home">
+            {animation ? (
+              <SplashPage />
+            ) : (
+              <MobileHome handleClick={handleClick} />
+            )}
           </div>
-        </div>
+        )}
       </IfNotAuthenticated>
     </>
   )
