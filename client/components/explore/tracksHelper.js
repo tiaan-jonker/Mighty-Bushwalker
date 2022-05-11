0a190bcfbface2541fdda77693d6084a709f8e31
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-11 15:39:31 +1200

    refactor updated tracks functions

--- client/components/explore/tracksHelper.js
+++ client/components/explore/tracksHelper.js
@@ -11,3 +11,52 @@ export function calculateDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
 function deg2rad(deg) {
   return deg * (Math.PI / 180)
 }
+
+export async function updateTracksData(tracks) {
+  const tracksWithPosition = await getDistanceAway(tracks)
+  const tracksUpdated = categoriseTrackLength(tracksWithPosition)
+  const unsavedTracks = tracksUpdated.filter(
+    (track) => !track.completed && !track.saved
+  )
+
+  unsavedTracks.sort((a, b) => {
+    return a.distanceAway - b.distanceAway
+  })
+  console.log(unsavedTracks)
+
+  return unsavedTracks
+}
+
+// categorise track length as short medium or long
+function categoriseTrackLength(tracks) {
+  const updatedTracks = tracks.map((track) => {
+    let lengthCategory = 'Long'
+    if (track.length < 5) {
+      lengthCategory = 'Short'
+    } else if (track.length < 12) {
+      lengthCategory = 'Medium'
+    }
+
+    return { ...track, lengthCategory }
+  })
+  return updatedTracks
+}
+
+function getDistanceAway(tracks) {
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const coords = position.coords
+      const updatedTracks = tracks.map((track) => {
+        const distanceAway = calculateDistanceBetweenPoints(
+          coords.latitude,
+          coords.longitude,
+          track.lat,
+          track.lon
+        ).toFixed(1)
+
+        return { ...track, distanceAway }
+      })
+      resolve(updatedTracks)
+    })
+  })
+}
